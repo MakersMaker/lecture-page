ae1d08e9bdd77d9deb220f9bc7cc1586f97b56d3
MUN SEONG UK <bulgogi1216@gmail.com>
2018-07-29 18:50:41 +0900

    seperate js.splitter file:

---
+++ assets/js/vendor/splitter/splitter.js
@@ -0,0 +1,20 @@
+jQuery(function ($) {
+  $('#left_middle_widget').width('100%').height('100%').split({
+    orientation: 'vertical',
+    limit: 10,
+    position: '30%',
+    percent: true
+  });
+  $('#middle_right_widget').split({
+    orientation: 'vertical',
+    limit: 10,
+    position: '60%',
+    percent: true
+  });
+  $('#right_upper_lower_widget').split({
+    orientation: 'horizontal',
+    limit: 10,
+    position: '35%',
+    percent: true
+  });
+});
